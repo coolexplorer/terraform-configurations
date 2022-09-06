2827c3a30cb2ce1bab4e639f21e91c01f2be587c
Allen Kim <seunkim@ea.com>
2022-09-05 21:49:58 -0700

    feat: added aks resource group and virtual network

--- utils/network/main.tf
+++ utils/network/main.tf
@@ -1,27 +1,49 @@
 resource "random_string" "random" {
-  length = 4
+  length  = 4
   numeric = true
   special = false
 }
 
-resource "azurerm_virtual_network" "test_vnet" {
-  name = "vnet-${random_string.random.result}"
-  location = var.location
+resource "azurerm_virtual_network" "vm_vnet" {
+  name                = "vnet-vm-${random_string.random.result}"
+  location            = var.location
   resource_group_name = var.network_rg_name
-  address_space = ["10.0.0.0/26"]
+  address_space       = ["10.0.0.0/26"]
 
   subnet {
-    name = "subnet1"
+    name           = "subnet1"
     address_prefix = "10.0.0.0/27"
   }
 
   subnet {
-    name = "subnet2"
+    name           = "subnet2"
     address_prefix = "10.0.0.32/27"
   }
 
   tags = {
-    createdBy = "Allen"
+    createdBy   = var.owner
+    environment = "test"
+  }
+}
+
+resource "azurerm_virtual_network" "aks_vnet" {
+  name                = "vnet-aks-${random_string.random.result}"
+  location            = var.location
+  resource_group_name = var.network_rg_name
+  address_space       = ["10.1.0.0/21"]
+
+  subnet {
+    name           = "subnet1"
+    address_prefix = "10.1.0.0/22"
+  }
+
+  subnet {
+    name           = "subnet2"
+    address_prefix = "10.1.4.0/22"
+  }
+
+  tags = {
+    createdBy   = var.owner
     environment = "test"
   }
 }
--- utils/resource_groups/main.tf
+++ utils/resource_groups/main.tf
@@ -1,15 +1,20 @@
 resource "random_string" "random" {
-  length = 4
+  length  = 4
   numeric = true
   special = false
 }
 
 resource "azurerm_resource_group" "network_rg" {
-  name = "network-rg-${random_string.random.result}"
+  name     = "network-rg-${random_string.random.result}"
   location = var.location
 }
 
 resource "azurerm_resource_group" "compute_rg" {
-  name = "compute-rg-${random_string.random.result}"
+  name     = "compute-rg-${random_string.random.result}"
+  location = var.location
+}
+
+resource "azurerm_resource_group" "aks_rg" {
+  name     = "aks-rg-${random_string.random.result}"
   location = var.location
 }
